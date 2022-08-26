d6ceb2f58af95fb340b8c2fc6efc7925f9a8c48f
Josef Bnm <ssefbnm@gmail.com>
2022-08-26 18:32:22 +0100

    add hero features & hero sections

---
+++ src/components/Business.jsx
@@ -0,0 +1,12 @@
+import { features } from "../constants"
+import styles, { layout } from "../style"
+import Button from './Button'
+
+const Business = () => (
+  <section>
+    
+  </section>
+)
+  
+
+export default Business
---
+++ src/components/Clients.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Clients() {
+  return (
+    <div>Client</div>
+  )
+}
+
+export default Clients
